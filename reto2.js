f4fd0f3984dc7ce08675904bd1a9b919b46b9435
Chrystian Calderon <chrystianralfc@gmail.com>
2025-12-02 22:52:03 -0400

    solucion al reto 2

---
+++ reto2.js
@@ -0,0 +1,37 @@
+/**
+ * @param {Array<{ toy: string, quantity: number }>} giftsToProduce
+ * @returns {string[]} Array of manufactured gifts
+ */
+function manufactureGifts(giftsToProduce) {
+  let giftProduced = [];
+  for (let gift of giftsToProduce) {
+    for (let i = 0; i < gift.quantity; i++) {
+      giftProduced.push(gift.toy);
+    }
+  }
+  return giftProduced;
+}
+
+const production1 = [
+  { toy: 'car', quantity: 3 },
+  { toy: 'doll', quantity: 1 },
+  { toy: 'ball', quantity: 2 }
+]
+
+const result1 = manufactureGifts(production1)
+console.log(result1)
+
+const production2 = [
+  { toy: 'train', quantity: 0 }, // no se fabrica
+  { toy: 'bear', quantity: -2 }, // tampoco
+  { toy: 'puzzle', quantity: 1 }
+]
+
+const result2 = manufactureGifts(production2)
+console.log(result2)
+// ['puzzle']
+
+const production3 = []
+const result3 = manufactureGifts(production3)
+console.log(result3)
+// []
